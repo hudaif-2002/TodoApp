a2a1623e2382f6ede9a53abc6f1a303b3748fbce
hudaif-eg <haham@eg.dk>
2025-12-30 01:13:04 +0530

    Use netlify.toml redirects with force=false for proper SPA routing

--- obj/Release/net9.0/TodoApp.AssemblyInfo.cs
+++ obj/Release/net9.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a85f7af0d13b7be390d4e78e60c7ba64031cb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
